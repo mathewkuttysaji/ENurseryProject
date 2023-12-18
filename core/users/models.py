c1e957d5cffdc4c709b6e665b9fc0eda443717e7
Mathewkutty Saji <mathewkuttysaji2002@gmail.com>
2023-12-18 23:01:18 +0530

    ContactUs model created and migrated

--- core/users/models.py
+++ core/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+from accounts.models import Customers
 # Create your models here.
+
+class ContactUs(models.Model):
+    customer = models.ForeignKey(Customers, on_delete=models.CASCADE) 
+    date = models.DateTimeField(auto_now_add=True) 
+    message_text = models.TextField()
