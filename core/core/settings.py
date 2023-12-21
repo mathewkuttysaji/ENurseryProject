63ae1d68762b921692eef56f5498f71cf4757378
Mathewkutty Saji <mathewkuttysaji2002@gmail.com>
2023-12-21 16:48:29 +0530

    Updated setting.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -134,4 +134,9 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'mathewkuttysaji2002@gmail.com'
-EMAIL_HOST_PASSWORD = 'xsry sivv dytv wjds'
+EMAIL_HOST_PASSWORD = 'xsry sivv dytv wjds' 
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+MEDIA_URL = '/media/' 
+
+LOGIN_URL = '/login'
